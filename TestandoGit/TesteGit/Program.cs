c9700b0c2d62a300f759bc3abec5918bf938357d
Anderson Alves <alves.hu@gmail.com>
2020-03-03 11:04:54 -0300

    #02 Modificado

--- TestandoGit/TesteGit/Program.cs
+++ TestandoGit/TesteGit/Program.cs
@@ -7,6 +7,11 @@ namespace TesteGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+                        Console.WriteLine(" 4 ");
+                        Console.WriteLine(" 3 ");
+                        Console.WriteLine(" 2 ");
+                        Console.WriteLine(" 1 ");
+
         }
     }
 }
